04c5dcaf010547a6b82fe44a7a0827094eeae2db
Madhava Di Florio <mattdiflorio@gmail.com>
2019-01-01 21:37:39 +0100

    Initial recipe list.

---
+++ src/assets/recipes.js
@@ -0,0 +1,170 @@
+export default [
+  {
+    name: "Zucchini lasagne",
+    ingredients: [
+      { type: "Vegetables", name: "Zucchini", amount: 4 },
+      { type: "Cans", name: "Tomato sauce in a can", amount: 1.5 },
+      { type: "Refrigerated", name: "Mozzarella", amount: 1 },
+      { type: "Vegetables", name: "Garlic", amount: 3 },
+      { type: "Refrigerated", name: "Parmigiano", amount: 1 },
+      { type: "Spices", name: "Basil", amount: 1 },
+      { type: "Spices", name: "Origano", amount: 1 }
+    ]
+  },
+  {
+    name: "Tortillas",
+    ingredients: [
+      { type: "Bakery", name: "Tortillas", amount: 1 },
+      { type: "Refrigerated", name: "Tomato sauce in a can", amount: 1 },
+      { type: "Refrigerated", name: "Beans in a can", amount: 1 },
+      { type: "Refrigerated", name: "Corn in a can", amount: 1 },
+      { type: "Refrigerated", name: "Tomato concentrate", amount: 1 },
+      { type: "Refrigerated", name: "Gruyère rapé", amount: 1 },
+      { type: "Spices", name: "Paprika", amount: 1 },
+      { type: "Vegetables", name: "Parsley", amount: 1 },
+      { type: "Vegetables", name: "Avocado", amount: 1 },
+      { type: "Refrigerated", name: "Sour cream", amount: 1 },
+      { type: "Vegetables", name: "Lemon", amount: 1 }
+    ]
+  },
+  {
+    name: "Mushroom Ricotta Spinach Pie",
+    ingredients: [
+      { type: "Refrigerated", name: "Pate feuillete", amount: 2 },
+      { type: "Vegetables", name: "Mushrooms", amount: 1 },
+      { type: "Vegetables", name: "Spinach", amount: 1 },
+      { type: "Refrigerated", name: "Ricotta", amount: 1 },
+      { type: "Spices", name: "Nutmeg", amount: 1 }
+    ]
+  },
+  {
+    name: "Spaghetti with Garlic and Chilli",
+    ingredients: [
+      { type: "Pasta", name: "Spaghetti", amount: 1 },
+      { type: "Vegetables", name: "Chilli", amount: 1 },
+      { type: "Vegetables", name: "Garlic", amount: 4 },
+      { type: "Refrigerated", name: "(Optional) Parmigiano", amount: 1 }
+    ]
+  },
+  {
+    name: "Spaghetti with Garlic and Chilli",
+    ingredients: [
+      { type: "Pasta", name: "Spaghetti", amount: 1 },
+      { type: "Vegetables", name: "Chilli", amount: 1 },
+      { type: "Vegetables", name: "Garlic", amount: 4 },
+      { type: "Refrigerated", name: "(Optional) Parmigiano", amount: 1 }
+    ]
+  },
+  {
+    name: "Lebanese Meze",
+    ingredients: [
+      { type: "Flour", name: "Flour", amount: 1 },
+      { type: "Flour", name: "Yeast", amount: 1 },
+      { type: "Vegetables", name: "Eggplant", amount: 1 },
+      { type: "Refrigerated", name: "Greek Yogurt", amount: 1 },
+      { type: "Vegetables", name: "Lemon juice", amount: 1 },
+      { type: "Bio", name: "Pine nuts", amount: 1 },
+      { type: "Vegetables", name: "Mint", amount: 1 },
+      { type: "Vegetables", name: "Parsley", amount: 1 },
+      { type: "Bio", name: "Sesame seeds", amount: 1 },
+      { type: "Flour", name: "Spoon of sugar", amount: 1 }
+    ]
+  },
+  {
+    name: "Potato bake (Cat version)",
+    ingredients: [
+      { type: "Vegetables", name: "Potatoes", amount: 5 },
+      { type: "Vegetables", name: "Garlic", amount: 4 },
+      { type: "Refrigerated", name: "Milk", amount: 1.5 },
+      { type: "Refrigerated", name: "Cream", amount: 1 },
+      { type: "Spices", name: "Nutmeg", amount: 1 },
+      { type: "Vegetables", name: "Ciboulette", amount: 1 }
+    ]
+  },
+  {
+    name: "Potato bake (Douds mums version)",
+    ingredients: [
+      { type: "Vegetables", name: "Potatoes", amount: 5 },
+      { type: "Vegetables", name: "Garlic", amount: 4 },
+      { type: "Refrigerated", name: "Milk", amount: 1.5 },
+      { type: "Refrigerated", name: "Mozzarella", amount: 1 },
+      { type: "Spices", name: "Tumeric", amount: 1 }
+    ]
+  },
+  {
+    name: "Vegetable bake with Vegie burger / steak",
+    ingredients: [
+      { type: "Vegetables", name: "Zucchini", amount: 2 },
+      { type: "Vegetables", name: "Tomatoes", amount: 5 },
+      { type: "Refrigerated", name: "Mozzarella", amount: 1 },
+      { type: "Vegetables", name: "Garlic", amount: 3 },
+      { type: "Vegetables", name: "Ciboulette", amount: 1 },
+      { type: "Spices", name: "Origano", amount: 1 },
+      { type: "Cans", name: "Can of beans", amount: 1 },
+      { type: "Flour", name: "Flour", amount: 0.1 },
+      { type: "Spices", name: "Spices", amount: 1 },
+      { type: "Refrigerated", name: "Steak", amount: 1 }
+    ]
+  },
+  {
+    name: "Mushroom soup",
+    ingredients: [
+      { type: "Vegetables", name: "Mushrooms", amount: 1 },
+      { type: "Refrigerated", name: "Butter", amount: 1 },
+      { type: "Soup", name: "Vegetable stock cube", amount: 1 },
+      { type: "Refrigerated", name: "Cream", amount: 1 },
+      { type: "Vegetables", name: "Ciboulette / Parsley", amount: 1 },
+      { type: "Bakery", name: "Baguette", amount: 1 },
+      { type: "Refrigerated", name: "Cheese", amount: 1 }
+    ]
+  },
+  {
+    name: "Potato and coconut milk soup",
+    ingredients: [
+      { type: "Vegetables", name: "Potatoes", amount: 3 },
+      { type: "Asian", name: "Coconut milk in can", amount: 1 },
+      { type: "Soup", name: "Vegetable stock cube", amount: 1 },
+      { type: "Spices", name: "Spices", amount: 1 },
+      { type: "Bakery", name: "Baguette", amount: 1 },
+      { type: "Refrigerated", name: "Cheese", amount: 1 }
+    ]
+  },
+  {
+    name: "Pasta with vegetables",
+    ingredients: [
+      { type: "Pasta", name: "Pasta", amount: 1 },
+      { type: "Vegetables", name: "Tomatoes", amount: 1 },
+      { type: "Vegetables", name: "Mushrooms / Zucchinis", amount: 1 },
+      { type: "Vegetables", name: "Garlic", amount: 3 },
+      { type: "Spices", name: "Spices", amount: 1 }
+    ]
+  },
+  {
+    name: "Noodles stir-fry",
+    ingredients: [
+      { type: "Pasta", name: "Rice or Wheat Noodles", amount: 1 },
+      { type: "Vegetables", name: "Mix of Asian Vegetables", amount: 1 },
+      { type: "Vegetables", name: "Mushrooms", amount: 1 },
+      { type: "Asian", name: "Soya Sauce", amount: 1 },
+      { type: "Vegetables", name: "Ginger", amount: 1 },
+      { type: "Vegetables", name: "Garlic", amount: 2 },
+      { type: "Vegetables", name: "Coriander", amount: 2 },
+      { type: "Vegetables", name: "Chilli", amount: 1 }
+    ]
+  },
+  {
+    name: "Noodles Soup",
+    ingredients: [
+      { type: "Asian", name: "Rice Noodles", amount: 1 },
+      { type: "Vegetables", name: "Chinese Mushrooms", amount: 1 },
+      { type: "Vegetables", name: "Carrots", amount: 1 },
+      { type: "Vegetables", name: "Radish", amount: 2 },
+      { type: "Asian", name: "Soya Sauce", amount: 1 },
+      { type: "Vegetables", name: "Ginger", amount: 1 },
+      { type: "Vegetables", name: "Garlic", amount: 2 },
+      { type: "Vegetables", name: "Coriander", amount: 2 },
+      { type: "Vegetables", name: "Chilli", amount: 1 },
+      { type: "Vegetables", name: "Haricots coco", amount: 1 }
+    ]
+  }
+];
